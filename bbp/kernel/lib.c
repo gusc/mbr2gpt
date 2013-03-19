1e371a59bf22e03a7c357015e1fe82711b224b04
Gusts 'gusC' Kaksis <gusts.kaksis@gmail.com>
2013-03-19 21:46:33 +0200

    Forgot to add Tiny C Lib source :)

---
+++ bbp/kernel/lib.c
@@ -0,0 +1,223 @@
+/*
+
+Tiny helper function library
+============================
+
+TOC:
+	* Memory manipulation functions
+	* Character string functions
+
+License (BSD-3)
+===============
+
+Copyright (c) 2012, Gusts 'gusC' Kaksis <gusts.kaksis@gmail.com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the <organization> nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#include "lib.h"
+#include "common.h"
+
+//
+// Memory manipulation functions
+//
+
+void mem_copy(uint8 *dest, uint64 len, const uint8 *src){
+	while(len--){
+		*dest++ = *src++;
+	}
+}
+void mem_fill(uint8 *dest, uint64 len, uint8 val){
+	while(len--){
+		*dest++ = val;
+	}
+}
+bool mem_compare(const uint8 *buff1, const uint8 *buff2, uint64 len){
+	while (len--){
+		if (*(buff1++) != *(buff2++)){
+			return false;
+		}
+	}
+	return true;
+}
+
+//
+// String functions
+//
+
+#define MAX_INT_STR 32
+
+uint32 str_copy(char *dest, uint64 len, const char *src){
+	char *sp = (char *)src;
+	char *dp = (char *)dest;
+	uint64 s = 0;
+	while (*sp != 0 && len != 0){
+		// Copy till the end of source or destination has been reached
+		*(dp++) = *(sp++);
+		len --;
+		s ++;
+	}
+	return s;
+}
+uint64 str_length(const char *str){
+	uint64 len = 0;
+	while (*(str++) != 0){
+		// Increment till the end of string
+		len ++;
+	}
+	return len;
+}
+int64 str_char_idx(const char *haystack, const char needle, uint64 offset){
+	int64 idx = 0;
+	// Move offset
+	while (offset--){
+		if (*(haystack++) == 0){
+			// End of the string
+			return -1;
+		}
+	}
+	while (*haystack != 0){
+		if (*haystack == needle){
+			// Found an occurence
+			return idx;
+		}
+		idx++;
+		haystack++;
+	}
+	return -1;
+}
+uint64 int_to_str(char *dest, uint64 len, int64 val, int64 base){
+	static char tmp[MAX_INT_STR + 1];
+	mem_fill((uint8 *)tmp, MAX_INT_STR + 1, 0);
+	char *b = (char *)tmp + MAX_INT_STR;
+	char c = 0;
+	if (val >= 0){
+		// Process positive value
+		do {
+			c = (char)(val % base);
+			if (c > 9){
+				*--b = 'A' + (c - 10);
+			} else {
+				*--b = '0' + c;
+			}
+			val /= base;
+		} while (val != 0);
+	} else if (val < 0){
+		// Process negative value
+		do {
+			c = (char)(val % base);
+			if (c < -9){
+				*--b = 'A' - (c + 10);
+			} else {
+				*--b = '0' - c;
+			}
+			val /= base;
+		} while (val != 0);
+		*--b = '-';
+	}
+	// Copy the result and return number of digits processed
+	return str_copy(dest, len, b);
+}
+int64 str_to_int(const char *str, int64 base){
+	char *b = (char *)str;
+	int64 val = 0;
+	uint8 negative = 0;
+	// Trim leading spaces
+	while (*b == 0x20 || *b == 0x09){ // space || tab
+		b++;
+	}
+	// Check if a negative number
+	if (*b == '-'){
+		negative = 1;
+	}
+	// Calculate integer value
+	while (*b >= 0x30 && *b <= 0x39){
+		val *= 10; // multiply upwwards
+		val += (*b) - 0x30; // add current digit
+		b ++;
+	}
+	// Set minus sign if necessary
+	if (negative){
+		return 0 - val;
+	}
+	return val;
+}
+uint64 str_write_f(char *dest, uint64 len, const char *format, ...){
+	va_list args;
+	va_start(args, format);
+	uint64 ret = __write_f(dest, len, format, args);
+	va_end(args);
+	return ret;
+}
+uint64 str_read_f(const char *src, const char *format, ...){
+	va_list args;
+	va_start(args, format);
+	uint64 ret = __read_f(src, format, args);
+	va_end(args);
+	return ret;
+}
+
+
+//
+// Private functions
+//
+
+uint64 __write_f(char *dest, uint64 len, const char *format, va_list args){
+	uint64 fm_len = str_length(format);
+	uint64 ret = 0;
+	uint8 a = 0;
+	char *f = (char *)format;
+	while (len-- && fm_len--){
+		if (*f == '%'){ // possible specifier
+			switch (*(f + 1)){
+				case 'd': // integer
+					
+					break;
+				case 'u': // unsigned integer
+					
+					break;
+				case 'x': // hex
+
+					break;
+				case 's': // string
+
+					break;
+				default: // not a specifier
+					*(dest++) = *f;
+					*(dest++) = *(f + 1);
+					break;
+			}
+			f += 2;
+		} else { // copy string
+			*(dest++)=*(f++);
+		}
+		ret ++;
+	}
+	return ret;
+}
+uint64 __read_f(const char *src, const char *format, va_list args){
+
+	return 0;
+}
---
+++ bbp/kernel/lib.h
@@ -0,0 +1,152 @@
+/*
+
+Tiny helper function library
+============================
+
+TOC:
+	* Memory manipulation functions
+	* Character string functions
+
+Function argument order explanation. Basically it should sound like this:
+"Fill this bucket for that many items of that other bucket", thus it's like
+the Intel ASM syntax of "instruction destination, source" whith an extra size
+specifier, so to prevent buffer overflows etc.
+
+License (BSD-3)
+===============
+
+Copyright (c) 2012, Gusts 'gusC' Kaksis <gusts.kaksis@gmail.com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the <organization> nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#ifndef __lib_h
+#define __lib_h
+
+#include "common.h"
+
+#define INT_BASE_BIN 2
+#define INT_BASE_OCT 8
+#define INT_BASE_DEC 10
+#define INT_BASE_HEX 16
+
+//
+// Memory manipulation functions
+//
+
+/**
+* Copy data from one memory location to another
+* @param [out] dest - destination memory
+* @param [in] src - source memory
+* @param len - number of bytes to copy
+* @return void
+*/
+void mem_copy(uint8 *dest, uint64 len, const uint8 *src);
+/**
+* Fill a memory buffer with a single byte value
+* @param [out] dest - destination memory
+* @param [in] val - value to set
+* @param len - number of bytes to set
+* @return void
+*/
+void mem_fill(uint8 *dest, uint64 len, uint8 val);
+/**
+* Compare two memory regions
+* @param [in] buff1
+* @param [in] buff2
+* @param len - comparison length
+* @return true if they are equal, false if not
+*/
+bool mem_compare(const uint8 *buff1, const uint8 *buff2, uint64 len);
+
+//
+// String functions
+//
+
+/**
+* Copy a string from one buffer to another
+* @param [out] dest - destination buffer
+* @param len - destination buffer length
+* @param [in] src - source buffer
+* @return number of characters copied
+*/
+uint32 str_copy(char *dest, uint64 len, const char *src);
+/**
+* Calculate the length of a string
+* @param [in] str - string
+* @return string length
+*/
+uint64 str_length(const char *str);
+/**
+* Search for a character in a string
+* @param [in] haystack - source buffer
+* @param needle - character to find
+* @param offset - number of characters to offset from the beginning of haystack
+* @return character index (-1 if not found)
+*/
+int64 str_char_idx(const char *haystack, const char needle, uint64 offset);
+/**
+* Convert integer to string
+* @param [out] dest - destination buffer
+* @param len - destination buffer length
+* @param in - input integer
+* @param base - the base of the integer represented by the string (8, 10 or 16)
+* @return number of characters in a string representation
+*/
+uint64 int_to_str(char *dest, uint64 len, int64 val, int64 base);
+/**
+* Convert a string to integer
+* @param [in] str - integer string
+* @param base - the base of the integer represented by the string (8, 10 or 16)
+* @return output integer
+*/
+int64 str_to_int(const char *str, int64 base);
+/**
+* Build a formated string from a format and additional parameters
+* @param [out] dest - destination string pointer
+* @param len - maximum length allowed to fill
+* @param [in] format - standard C printf format string
+* @param [in] ... - additional arguments
+* @return number of characters written
+*/
+uint64 str_write_f(char *dest, uint64 len, const char *format, ...);
+/**
+* Parse a formated string using a format and store results in additional parameters
+* @param [in] src - source string pointer
+* @param [in] format - standard C printf format string
+* @param [out] ... - additional argument pointers
+* @return number of arguments parsed
+*/
+uint64 str_read_f(const char *src, const char *format, ...);
+
+//
+// Private functions
+//
+
+uint64 __write_f(char *dest, uint64 len, const char *format, va_list args);
+uint64 __read_f(const char *src, const char *format, va_list args);
+
+
+#endif /* __lib_h */
